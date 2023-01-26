d71d383491c1371af477827b8ee0e3918b0d2101
JoseaSosa <josea_1998@htomail.com>
2023-01-26 12:09:10 -0500

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args){
+
+   //EN EL MAIN LLAMO A MIS CLASES
+   //EN EL MAIN CREO LOS OBJETOS DE MIS CLASES
+   //EN EL MAIN CREO UNA INSTANCIA DE MI CLASE
+   //EN EL MAIN LE SACO COPIA A MIS CLASES Y CREO OBJETOS
+
+     Invitado objetoDeClaseInvitado = new Invitado();
+     Invitado objeto2=new Invitado(33,true,"jose",10000);
+
+
+
+        System.out.println(objetoDeClaseInvitado.nombre);
+        System.out.println(objeto2.nombre);
+    }
+}
